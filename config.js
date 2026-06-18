5dff1ee0bdd66976ebe128fabda140b4ffc9aa1a
Shaik Sohail <shaiksohaildnalabsindia@gmail.com>
2026-06-18 11:32:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,13 @@ const CONFIG = {
     SCANNER_API_URL: "https://script.google.com/macros/s/AKfycbyDie30z8_YE8HbZXobIKVXles9RFW3Xo5k1NE24DzrfnTZkfks6u6e4_ZKYFmumrrV/exec",
     TRACKING_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz0eXdOvzB5zVGRNkQkCnCmLixnZdH3E-uXpZRkv4nIZfqMDmOcuU1DvcFGVshO3yyG/exec",
     SCANNER_SAVE_URL: "https://script.google.com/macros/s/AKfycbwiTXXO1ypYhs-fykSl9apgTV4ZM9sGvdK5uKp-hkxGYcJjCJoaNZzsuXXFouq9ziY/exec",
-    
-    // NEW: Image Upload Endpoints
     UPLOAD_TRACKING_URL: "https://script.google.com/macros/s/AKfycbxrAMIt5g_szYTWAXFAACOI7R-XpJiP9CS9bGEsX5bja552Ic-EoAJwbBSN99ZTwdyU_A/exec",
     UPLOAD_PATIENT_URL: "https://script.google.com/macros/s/AKfycbxB5-Po-A-N70dR7Ie9VXP0QDHw1hFARAhaMTDoD4FbcNbDrPdh4W1nWootW1iepmtWbA/exec",
+    DELIVERED_TODAY_API: "https://script.google.com/macros/s/AKfycby9f86QjvNESTF8Rm0oYfaZEMOvpCjyQ_TZL1id8lkLZu0Ck1qYPIhrSwVbxYuxdSUe7g/exec",
+    IMAGE_API: "https://script.google.com/macros/s/AKfycbwZw152zyjIA1gU5Ssn4YRl8kTXrdaDlMB8XzZWHc6Hb4WNI7C6D5W4xH-sRI0lE_ZNTA/exec",
+    
+    // NEW: Gallery Endpoint
+    GALLERY_API_URL: "https://script.google.com/macros/s/AKfycbxC0SywOr5T6ra5kFbwO9hC8jfb4ImECZbMSXAlTSadie7Yv4IMTNi5T_TCf6X5GY-LFA/exec",
     
     VERSION: "2.1.0"
 };
