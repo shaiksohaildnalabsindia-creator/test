07178d4bbee821694c038c5be23a1f78ffead93f
Shaik Sohail <shaiksohaildnalabsindia@gmail.com>
2026-06-18 10:57:32 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const CONFIG = {
+    // Your Google Apps Script Web App URL
+    API_URL: "https://script.google.com/macros/s/AKfycbxrtcZeaWfHYcTfEhidGXr3mxzmrUTaN-zliEJUGlbb8kBl48gby01-mEHxEuV2WlFzrA/exec",
+    
+    // You can add other global variables here in the future
+    VERSION: "2.1.0"
+};
